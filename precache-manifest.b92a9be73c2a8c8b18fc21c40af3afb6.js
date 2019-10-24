443e365c1622a0f791573c00a248b536a8a929a8
vigneshenfin <vignesh@enfintechnologies.com>
2019-10-24 16:40:45 +0530

    new

---
+++ precache-manifest.b92a9be73c2a8c8b18fc21c40af3afb6.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f814a1e9f24d88f11e54c1d588f61a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2dfb4a64ce23268b0ccd",
+    "url": "/static/css/2.c51b6406.chunk.css"
+  },
+  {
+    "revision": "a13689788f4a0923cab1",
+    "url": "/static/css/main.82723330.chunk.css"
+  },
+  {
+    "revision": "2dfb4a64ce23268b0ccd",
+    "url": "/static/js/2.ca5c304d.chunk.js"
+  },
+  {
+    "revision": "a13689788f4a0923cab1",
+    "url": "/static/js/main.aeb48d27.chunk.js"
+  },
+  {
+    "revision": "47e7ddc50025565a2ca6",
+    "url": "/static/js/runtime-main.ab617505.js"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "2f99a85426a45e0c7f8707aae53af803",
+    "url": "/static/media/Raleway-Bold.2f99a854.ttf"
+  },
+  {
+    "revision": "353ea0a0ae707df315407d72fa507e2f",
+    "url": "/static/media/Raleway-Light.353ea0a0.ttf"
+  },
+  {
+    "revision": "818653e129689a969c72e0724f3a5169",
+    "url": "/static/media/Raleway-Medium.818653e1.ttf"
+  },
+  {
+    "revision": "84abe14c9756256a4b91300ba3e4ec62",
+    "url": "/static/media/Raleway-Regular.84abe14c.ttf"
+  },
+  {
+    "revision": "6ec58d1ea836b7ef69f94ea6770972a3",
+    "url": "/static/media/Raleway-SemiBold.6ec58d1e.ttf"
+  },
+  {
+    "revision": "c791c1700119dceeedba9576d87112d7",
+    "url": "/static/media/Raleway-Thin.c791c170.ttf"
+  },
+  {
+    "revision": "8964d7bc83d8e6fa8e0d40e6176fd515",
+    "url": "/static/media/bg.8964d7bc.png"
+  },
+  {
+    "revision": "9a467e290c871f65d7d0777d3e890f59",
+    "url": "/static/media/lapbook.9a467e29.png"
+  },
+  {
+    "revision": "ae14c331023a8e353daf4af89b8d37dc",
+    "url": "/static/media/logo.ae14c331.png"
+  }
+]);
